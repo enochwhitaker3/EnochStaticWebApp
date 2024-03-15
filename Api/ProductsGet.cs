e150af5bd93f14b0e5e22430c1d5e9f3574e51f0
enochwhitaker3 <enochwhitaker3@gmail.com>
2024-03-15 15:41:51 -0600

    new code new branch

--- Api/ProductsGet.cs
+++ Api/ProductsGet.cs
@@ -1,35 +1,25 @@
-using System;
-using System.IO;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
+using System.Threading.Tasks;
 
-namespace Api
+namespace Api;
+
+public class ProductsGet
 {
-    public static class ProductsGet
+    private readonly IProductData productData;
+
+    public ProductsGet(IProductData productData)
     {
-        [FunctionName("ProductsGet")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
-        {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+        this.productData = productData;
+    }
 
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
-        }
+    [FunctionName("ProductsGet")]
+    public async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "products")] HttpRequest req)
+    {
+        var products = await productData.GetProducts();
+        return new OkObjectResult(products);
     }
 }
